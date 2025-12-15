37cda2fc826706dfb0e9c376059f69a5f1a3ea84
Gahyun-313 <gus20313@gmail.com>
2025-12-16 03:24:24 +0900

    chore: 캐릭터 서브 화면 더미 스크린 추가

---
+++ src/screens/main/character/level/LevelCriteriaScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const LevelCriteriaScreen = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>LevelCriteriaScreen</Text>
+      <Text>레벨 기준 확인 (목록)</Text>
+    </View>
+  );
+};
+
+export default LevelCriteriaScreen;
