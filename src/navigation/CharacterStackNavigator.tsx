d14e9a5ee3cd63f52879a778597c4e3d38fb0714
Gahyun-313 <gus20313@gmail.com>
2025-12-16 02:57:34 +0900

    Revert "feat: 캐릭터 서브 화면 라우트 및 스택 등록"
    This reverts commit 47ce231f887d40e6ba3edc27319a81be0f9e62b5.

--- src/navigation/CharacterStackNavigator.tsx
+++ src/navigation/CharacterStackNavigator.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RouteNames} from '../../routes';
 import CharacterScreen from '../screens/main/CharacterScreen';
-import LevelCriteriaScreen from '../screens/main/character/level/LevelCriteriaScreen';
-import LevelCriteriaDetailScreen from '../screens/main/character/level/LevelCriteriaDetailScreen';
-import PointHistoryScreen from '../screens/main/character/history/PointHistoryScreen';
-import NotificationScreen from '../screens/main/character/notification/NotificationScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,23 +10,6 @@ const CharacterStackNavigator = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name={RouteNames.CHARACTER} component={CharacterScreen} />
       {/* 서브 화면들은 여기에 추가 */}
-
-      <Stack.Screen
-        name={RouteNames.CHARACTER_LEVEL}
-        component={LevelCriteriaScreen}
-      />
-      <Stack.Screen
-        name={RouteNames.CHARACTER_LEVEL_DETAIL}
-        component={LevelCriteriaDetailScreen}
-      />
-      <Stack.Screen
-        name={RouteNames.CHARACTER_POINT_HISTORY}
-        component={PointHistoryScreen}
-      />
-      <Stack.Screen
-        name={RouteNames.CHARACTER_NOTIFICATION}
-        component={NotificationScreen}
-      />
     </Stack.Navigator>
   );
 };
