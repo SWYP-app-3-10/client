9168e428e89063fe707150c32ac795647d3fbf66
Gahyun-313 <gus20313@gmail.com>
2025-12-16 02:27:39 +0900

    chore: 캐릭터 서브 화면 더미 파일 추가

---
+++ src/screens/main/character/level/LevelCriteriaDetailScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const LevelCriteriaDetailScreen = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>LevelCriteriaDetailScreen</Text>
+      <Text>레벨 기준 상세 (경험치 / 포인트)</Text>
+    </View>
+  );
+};
+
+export default LevelCriteriaDetailScreen;
