32cf51bc75cd3b46c80caf9390ddc7d35583545e
Gahyun-313 <gus20313@gmail.com>
2025-12-16 02:57:13 +0900

    Revert "feat: 캐릭터 화면에 서브 화면 이동 버튼 추가"
    This reverts commit 140d34108798234f8ff542d9dff79f45d703564a.

--- src/screens/main/CharacterScreen.tsx
+++ src/screens/main/CharacterScreen.tsx
@@ -1,76 +1,12 @@
-import React from 'react';
-import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import {RouteNames} from '../../../routes';
+import {Text, View} from 'react-native';
 
 const CharacterScreen = () => {
-  const navigation = useNavigation();
-
   return (
-    <View style={styles.container}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>CharacterScreen</Text>
       <Text>캐릭터 페이지</Text>
-
-      <View style={styles.buttonArea}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() =>
-            navigation.navigate(RouteNames.CHARACTER_LEVEL as never)
-          }>
-          <Text style={styles.buttonText}>레벨 기준 확인</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() =>
-            navigation.navigate(RouteNames.CHARACTER_LEVEL_DETAIL as never)
-          }>
-          <Text style={styles.buttonText}>레벨 기준 상세</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() =>
-            navigation.navigate(RouteNames.CHARACTER_POINT_HISTORY as never)
-          }>
-          <Text style={styles.buttonText}>포인트/경험치 내역</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() =>
-            navigation.navigate(RouteNames.CHARACTER_NOTIFICATION as never)
-          }>
-          <Text style={styles.buttonText}>알림</Text>
-        </TouchableOpacity>
-      </View>
     </View>
   );
 };
 
 export default CharacterScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  buttonArea: {
-    marginTop: 20,
-    width: '80%',
-  },
-  button: {
-    height: 44,
-    borderRadius: 10,
-    backgroundColor: '#111',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: '700',
-  },
-});
