394e1cb42731b57592be4cb64652afd951184d3c
bangmim <amd_b@naver.com>
2026-01-10 00:18:44 +0900

    feat: 아이콘 추가 & 수정

--- src/icons/commonIcons/commonIcons.tsx
+++ src/icons/commonIcons/commonIcons.tsx
@@ -29,10 +29,13 @@ import Note from '../../assets/svg/note.svg';
 import Clock from '../../assets/svg/clock.svg';
 import NoArticles from '../../assets/svg/noArticles.svg';
 import Search_tab from '../../assets/svg/search_tab.svg';
+import View from '../../assets/svg/View.svg';
+
 export const Ic_backIcon = createIconComponent(Icon_back, ICON_SIZES.XL);
 export const CircleIcon = createIconComponent(Circle, scaleWidth(14));
 export const InfoIcon = createIconComponent(Info, ICON_SIZES.L);
-export const CloseIcon = createIconComponent(Close, ICON_SIZES.L);
+export const CloseIcon = createIconComponent(Close, scaleWidth(28));
+export const ViewIcon = createIconComponent(View, scaleWidth(18));
 export const TriangleIcon = createIconComponent(Triangle, ICON_SIZES.X3L);
 export const AppleIcon = createIconComponent(Apple, ICON_SIZES.M);
 export const GoogleIcon = createIconComponent(Google, ICON_SIZES.M);
