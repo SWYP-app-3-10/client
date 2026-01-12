4e21d82d8ee5241e80164600a42daf0c24f583ae
bangmim <amd_b@naver.com>
2026-01-12 23:04:34 +0900

    feat: 버전 업데이트 완료(?)

--- metro.config.js
+++ metro.config.js
@@ -6,21 +6,22 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-// const config = {
-//   transformer: {
-//     getTransformOptions: async () => ({
-//       transform: {
-//         experimentalImportSupport: false,
-//         inlineRequires: true,
-//       },
-//     }),
-//   },
-//   resolver: {
-//     assetExts: getDefaultConfig(__dirname).resolver.assetExts.filter(
-//       ext => ext !== 'svg',
-//     ),
-//     sourceExts: [...getDefaultConfig(__dirname).resolver.sourceExts, 'svg'],
-//   },
-// };
-const config = {};
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+    transformer: {
+        babelTransformerPath: require.resolve('react-native-svg-transformer'),
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+    },
+    resolver: {
+        assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+        sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+    },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   project: {
     ios: {},
-    android: {},
+    android: {
+      sourceDir: './android',
+      appName: 'app',
+      packageName: 'io.neurous.app',
+    },
   },
   assets: ['./ios/', './android/app/src/main/assets/fonts/'],
 };
