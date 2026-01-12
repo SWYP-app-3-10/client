14c8fc2e5b34800f0bcc545b9ec6c0e4a10f9769
bangmim <amd_b@naver.com>
2026-01-13 03:34:11 +0900

    feat: ios 버전 맞추는중

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -1,5 +1,4 @@
 #import "AppDelegate.h"
-
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -7,19 +6,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"Neurous";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-  return [self getBundleURL];
-}
-
-- (NSURL *)getBundleURL
+/// 🌟 [핵심 수정] 함수 이름을 'bundleURL'로 정확히 맞춰야 합니다.
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
